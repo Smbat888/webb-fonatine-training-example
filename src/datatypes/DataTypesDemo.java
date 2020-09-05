ef8668039099dee03e31f954864ccbcf9e13d342
Smbat Sargsyan <smbatsargsyan1992@gmail.com>
2020-09-05 11:01:33 +0400

    Added data types package.

---
+++ src/datatypes/DataTypesDemo.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class DataTypesDemo {
+
+    /*
+       ?? -> no git file
+       A -> Added to git
+       M -> git file modified
+
+
+       git commit --> ....
+
+       git push --> move to remote repo
+
+
+
+     */
+
+}
